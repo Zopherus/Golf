698943287638dfda8a7e3b29601027eab18c0bb5
Eric Zhu <Eric Zhu>
2017-04-10 22:40:28 -0700

    commit

--- app/src/main/java/com/example/ejz19/golf/Menu.java
+++ app/src/main/java/com/example/ejz19/golf/Menu.java
@@ -1,8 +1,10 @@
 package com.example.ejz19.golf;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
@@ -18,5 +20,23 @@ public class Menu extends AppCompatActivity {
         button.setText("Players");
         button = (Button) findViewById(R.id.Courses);
         button.setText("Courses");
+    }
+
+    public void StartGame(View view)
+    {
+        Intent intent = new Intent(this, ChoosePlayers.class);
+        startActivity(intent);
+    }
+
+    public void Players(View view)
+    {
+        Intent intent = new Intent(this, Players.class);
+        startActivity(intent);
+    }
+
+    public void Courses(View view)
+    {
+        Intent intent = new Intent(this, Courses.class);
+        startActivity(intent);
     }
 }
